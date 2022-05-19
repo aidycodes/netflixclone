b3376e61d6bc4fe041b46309eb6674a52d81a1cc
Aidy <aidycodes@gmail.com>
2022-05-19 14:05:49 +0100

    implemented SSR for main page

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,19 @@ import Banner from '../components/banner/banner'
 import SectionCards from '../components/card/sectionCards'
 import Navbar from '../components/nav/navbar'
 import styles from '../styles/Home.module.css'
-import { getVideos } from '../lib/videos'
+import { getVideos, getPopularVideos } from '../lib/videos'
 
 export const getServerSideProps = async() => {
-    const youtubeVideos = getVideos()
+    const youtubeVideos = await getVideos('Disney Trailers')
+    const productivityVideos = await getVideos('Productivity')
+    const travelVideos = await getVideos('Travel')
+    const popularVideos = await getPopularVideos()
     return {
-      props:{youtubeVideos}
+      props:{youtubeVideos, travelVideos, productivityVideos, popularVideos}
     }
 }
 
-export default function Home({ youtubeVideos }) {
+export default function Home({ youtubeVideos, travelVideos, productivityVideos, popularVideos }) {
 
   return (
     <div className={styles.container}>
@@ -20,17 +23,22 @@ export default function Home({ youtubeVideos }) {
         <title>Netflix</title>
         <meta name="description" content="Video on demand" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>    
+      </Head> 
+ <div className={styles.main}>        
       <Navbar userName="{userName}"/>
       <Banner title='Clifford the red dog' subTitle='very cute dog' imgUrl="images/clifford.webp"/>
     <div className={styles.sectionWrapper}>
        <SectionCards title="Disney" size="large" videos={youtubeVideos}/>
     </div>
       <div className={styles.sectionWrapper}>
-       <SectionCards title="Cheese" size="medium" videos={youtubeVideos}/>
+       <SectionCards title="Travel" size="small" videos={travelVideos}/>
     </div>
       <div className={styles.sectionWrapper}>
-       <SectionCards title="Goats" size="small" videos={youtubeVideos}/>
+       <SectionCards title="Productivity" size="medium" videos={productivityVideos}/>
+    </div>
+          <div className={styles.sectionWrapper}>
+       <SectionCards title="Popular" size="small" videos={popularVideos}/>
+    </div>
     </div>
     </div>
   )
